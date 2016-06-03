5adeb3d9091bd179205caa9ee119bdf2124bb92c
Jo Apichai <jo@oozou.com>
2016-06-04 01:42:31 +0700

    Refactor code using object prototype

--- image-upload-preview.js
+++ image-upload-preview.js
@@ -1,78 +1,116 @@
 (function($) {
 
-  $.fn.imageUploadPreview = function() {
-    this.each(function() {
-      var self = $(this);
-      var input = self.find('input[type=file]');
-      var preview = self.find('.image-preview');
-      var newElems = self.find('.image-new');
-      var existsElems = self.find('.image-exists');
+  $.fn.extend({
+    imageUploadPreview: function(options) {
+      this.each(function() {
+        var $this = $(this);
+        var data = $this.data('image-upload-preview');
 
-      var setPreviewImage = function() {
-        if(input.prop("files")[0]) {
-          var reader = new FileReader();
-
-          reader.onload = function(e) {
-            var img = $('<img/>', { src: e.target.result })
-            preview.html(img);
-            newElems.hide();
-            existsElems.show();
-          }
-
-          reader.readAsDataURL(input.prop("files")[0])
-        } else {
-          existsElems.hide();
-          newElems.show();
+        if(!data) {
+          obj = new ImageUploadPreview(this, options);
+          $this.data('image-upload-preview', obj);
+          obj.init();
         }
-      }
+      });
 
-      var resetFileInput = function() {
-        preview.html('');
-        input.wrap('<form>').closest('form').get(0).reset();
-        input.unwrap();
-        existsElems.hide();
-        newElems.show();
-      }
+      return this;
+    }
+  });
 
-      if(preview.find('img').length > 0) {
-        existElems.show();
+  var ImageUploadPreview = function(element, options) {
+    if(!options || typeof options !== 'object') {
+      options = {};
+    }
+
+    this.options = $.extend(this._defaultOptions, options);
+
+    this.$self = $(element);
+    this.$input = this.$self.find('input[type=file]');
+    this.$previewContainer = this.$self.find(this.options.previewContainer);
+    this.$newImageNodes = this.$self.find(this.options.newImageNodes);
+    this.$existsImageNodes = this.$self.find(this.options.existsImageNodes);
+  }
+
+  ImageUploadPreview.prototype = {
+    init: function() {
+      if(this.$previewContainer.find('img').length > 0) {
+        this.$existsImageNodes.show();
       } else {
-        setPreviewImage()
-        newElems.show()
+        this._setPreviewImage();
+        this.$newImageNodes.show();
       }
+
+      this._bindEvents();
+    },
+
+    _defaultOptions: {
+      container: '.image-uploader',
+      previewContainer: '.image-preview',
+      uploadButton: '.image-trigger',
+      removeButton: '.image-remove',
+      newImageNodes: '.image-new',
+      existsImageNodes: '.image-exists'
+    },
+
+    _bindEvents: function() {
+      var self = this;
 
       // Trigger click on file input to open file selection window
-      self.on('click', '.image-trigger', function() {
-        input.click();
+      this.$self.on('click', this.options.uploadButton, function() {
+        self.$input.click();
       });
 
       // Show preview image when user select a file
-      input.on('change', function() {
-        if(input.prop("files")[0].name.match(/\.(jpg|jpeg|png|gif)$/)) {
-          setPreviewImage();
+      this.$input.on('change', function() {
+        if(self.$input.prop("files")[0].name.match(/\.(jpg|jpeg|png|gif)$/)) {
+          self._setPreviewImage();
         } else {
-          resetFileInput();
+          self._resetFileInput();
         }
       });
 
       // Remove preview image and show upload form when user click on remove button
-      self.on('click', '.image-remove', function() {
-        resetFileInput();
+      this.$self.on('click', this.options.removeButton, function() {
+        self._resetFileInput();
       });
 
       // Set file input when user drop image on uploader
-      self.on('dragover', function(e) {
-        input.css('display', 'block');
+      this.$self.on('dragover', function() {
+        self.$input.css('display', 'block');
       })
       .on('dragleave', function() {
-        input.css('display', 'none');
+        self.$input.css('display', 'none');
       })
-      .on('drop', function(e) {
-        input.css('display', 'none');
+      .on('drop', function() {
+        self.$input.css('display', 'none');
       });
-    });
+    },
+
+    _setPreviewImage: function() {
+      if(this.$input.prop("files")[0]) {
+        var self = this;
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          self.$previewContainer.html($('<img/>', { src: e.target.result }));
+          self.$newImageNodes.hide();
+          self.$existsImageNodes.show();
+        }
+
+        reader.readAsDataURL(this.$input.prop("files")[0]);
+      } else {
+        this.$existsImageNodes.hide();
+        this.$newImageNodes.show();
+      }
+    },
+
+    _resetFileInput: function() {
+      this.$previewContainer.html('');
+      this.$input.wrap('<form>').closest('form').get(0).reset();
+      this.$input.unwrap();
+      this.$existsImageNodes.hide();
+      this.$newImageNodes.show();
+    }
   }
 
-  return this;
-
-}( jQuery ));
+}(jQuery));
